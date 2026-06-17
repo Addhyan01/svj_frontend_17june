1c1cb1fe48115bb39fa528403607eb9ad9a2d334
Addhyan Kumar <addhyankumar@Addhyans-MacBook-Air.local>
2026-06-17 13:44:25 +0530

    404 error and chage url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v1',
-  withCredentials: false,
+  // baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v1',
+  // withCredentials: false,
+   baseURL: import.meta.env.VITE_API_BASE_URL 
 });
 
 // Attach JWT token to every request

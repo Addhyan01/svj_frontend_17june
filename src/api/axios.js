ee8b5f661e7174b99a31a6a4ae9d92a4fff9793e
Addhyan Kumar <addhyankumar@Addhyans-MacBook-Air.local>
2026-06-17 14:32:11 +0530

    add line

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API = axios.create({
   // baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v1',
   // withCredentials: false,
-   baseURL: import.meta.env.VITE_API_BASE_URL 
+  baseURL: import.meta.env.VITE_API_BASE_URL 
 });
 
 // Attach JWT token to every request
